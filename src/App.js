cb1e9a7b5cb3c6b4fe3d95a51ae9954f802e449d
danschoch <daniel.alexander.schoch@gmail.com>
2019-02-14 16:32:35 -0800

    confirm that fetch to bluehost WP api works

--- src/App.js
+++ src/App.js
@@ -3,6 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    posts: []
+  }
+
+  handleErrors = (response) => {
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+    return response;
+  }
+
+  
+
+  componentDidMount() {
+    fetch('https://pfc.xxr.mybluehost.me/wp-json/wp/v2/posts/1')
+    // .then(this.handleErrors)
+    .then(response => response.json() )
+    .then(response => { 
+      this.setState({ posts: this.state.posts.concat(response) });
+      console.log()
+    })
+    .catch(function(error) {
+      console.log('There has been a problem with your fetch operation: ', error.message);
+    });
+  }
+
+  renderPostTitle = () => {
+    return this.state.posts.map( post => {
+      console.log(post)
+      return(
+        <p key={post}>Title: {post.title.rendered} </p>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -20,9 +56,14 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <main id='post'>
+            {this.renderPostTitle()}
+        </main>
       </div>
     );
   }
+
+  
 }
 
 export default App;
